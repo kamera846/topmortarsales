c4d8ccc0260d453f194a9f228be91dd44e76b6f3
Mochammad Rafli Ramadani <keerouk.ink@gmail.com>
2023-06-19 17:55:55 +0700

    added new activity

---
+++ app/src/main/java/com/example/qontak/NewRoomChatFormActivity.kt
@@ -0,0 +1,11 @@
+package com.example.qontak
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class NewRoomChatFormActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_new_room_chat_form)
+    }
+}
