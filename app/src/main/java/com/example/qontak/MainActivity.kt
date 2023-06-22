bb70d7490e9f3fa291c011f5b75ba7908bd5a06d
Mochammad Rafli Ramadani <keerouk.ink@gmail.com>
2023-06-22 12:25:02 +0700

    setup search engine layout

--- app/src/main/java/com/example/qontak/MainActivity.kt
+++ app/src/main/java/com/example/qontak/MainActivity.kt
@@ -3,11 +3,15 @@ package com.example.qontak
 import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
+import android.os.Looper
+import android.text.Editable
+import android.text.TextWatcher
 import android.util.Log
 import android.view.MenuItem
 import android.view.View
 import android.view.animation.Animation
 import android.view.animation.AnimationUtils
+import android.widget.EditText
 import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.PopupMenu
@@ -38,9 +42,15 @@ class MainActivity : AppCompatActivity() {
     private lateinit var llTitleBar: LinearLayout
     private lateinit var llSearchBox: LinearLayout
     private lateinit var btnFab: FloatingActionButton
-    private lateinit var icSyncNow: ImageView
     private lateinit var icMore: ImageView
     private lateinit var icCloseSearch: ImageView
+    private lateinit var etSearchBox: EditText
+
+    // Initialize Search Engine
+    private val searchDelayMillis = 500L
+    private val searchHandler = Handler(Looper.getMainLooper())
+    private var searchRunnable: Runnable? = null
+    private var previousSearchTerm = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
@@ -54,6 +64,46 @@ class MainActivity : AppCompatActivity() {
         initClickHandler()
         loadingState(true)
         setRecyclerView()
+
+    }
+
+    private fun initVariable() {
+
+        rlLoading = findViewById(R.id.rl_loading)
+        rvListChat = findViewById(R.id.rv_chat_list)
+        llTitleBar = findViewById(R.id.title_bar)
+        llSearchBox = findViewById(R.id.search_box)
+        btnFab = findViewById(R.id.btn_fab)
+        icMore = findViewById(R.id.ic_more)
+        icCloseSearch = findViewById(R.id.ic_close_search)
+        etSearchBox = findViewById(R.id.et_search_box)
+
+        // Set Title Bar
+        icMore.visibility = View.VISIBLE
+
+    }
+
+    private fun initClickHandler() {
+
+        btnFab.setOnClickListener { navigateAddNewRoom() }
+        icMore.setOnClickListener { showPopupMenu() }
+        icCloseSearch.setOnClickListener { toggleSearchEvent(SEARCH_CLOSE) }
+
+    }
+
+    private fun loadingState(state: Boolean) {
+
+        if (state) {
+
+            rlLoading.visibility = View.VISIBLE
+            rvListChat.visibility = View.GONE
+
+        } else {
+
+            rlLoading.visibility = View.GONE
+            rvListChat.visibility = View.VISIBLE
+
+        }
 
     }
 
@@ -95,30 +145,6 @@ class MainActivity : AppCompatActivity() {
 
     }
 
-    private fun loadingState(state: Boolean) {
-
-        if (state) {
-
-            rlLoading.visibility = View.VISIBLE
-            rvListChat.visibility = View.GONE
-
-            icSyncNow.startAnimation(scaleAnimation)
-
-        } else {
-
-            rlLoading.visibility = View.GONE
-            rvListChat.visibility = View.VISIBLE
-
-            Handler().postDelayed({
-
-                icSyncNow.clearAnimation()
-
-            }, 500)
-
-        }
-
-    }
-
     private fun handleMessage(tag: String, message: String) {
 
         Log.d(tag, message)
@@ -128,43 +154,16 @@ class MainActivity : AppCompatActivity() {
 
     private fun navigateAddNewRoom() {
 
+        toggleSearchEvent(SEARCH_CLOSE)
+
         val intent = Intent(this, NewRoomChatFormActivity::class.java)
         startActivity(intent)
-
-    }
-
-    private fun initVariable() {
-
-        rlLoading = findViewById(R.id.rl_loading)
-        rvListChat = findViewById(R.id.rv_chat_list)
-        llTitleBar = findViewById(R.id.title_bar)
-        llSearchBox = findViewById(R.id.search_box)
-        btnFab = findViewById(R.id.btn_fab)
-        icSyncNow = findViewById(R.id.ic_sync_now)
-        icMore = findViewById(R.id.ic_more)
-        icCloseSearch = findViewById(R.id.ic_close_search)
-
-        // Set Title Bar
-//        icSyncNow.visibility = View.VISIBLE
-        icMore.visibility = View.VISIBLE
-
-    }
-
-    private fun initClickHandler() {
-
-        btnFab.setOnClickListener { navigateAddNewRoom() }
-//        icSyncNow.setOnClickListener { setRecyclerView() }
-        icMore.setOnClickListener { showPopupMenu() }
-        icCloseSearch.setOnClickListener { toggleSearchEvent(SEARCH_CLOSE) }
 
     }
 
     private fun showPopupMenu() {
         val popupMenu = PopupMenu(this@MainActivity, icMore)
         popupMenu.inflate(R.menu.option_main_menu)
-
-        // Hide search button
-        popupMenu.menu.findItem(R.id.option_search)?.isVisible = false
 
         popupMenu.setOnMenuItemClickListener { item: MenuItem ->
             when (item.itemId) {
@@ -184,25 +183,72 @@ class MainActivity : AppCompatActivity() {
 
     private fun toggleSearchEvent(state: String) {
 
+        val animationDuration = 500L
+        val slideInFromLeft = AnimationUtils.loadAnimation(this@MainActivity, R.anim.fade_slide_in_from_left)
+        slideInFromLeft.duration = animationDuration
+        val slideOutToRight = AnimationUtils.loadAnimation(this@MainActivity, R.anim.fade_slide_out_to_right)
+        slideOutToRight.duration = animationDuration
+        val slideInFromRight = AnimationUtils.loadAnimation(this@MainActivity, R.anim.fade_slide_in_from_right)
+        slideInFromRight.duration = animationDuration
+        val slideOutToLeft = AnimationUtils.loadAnimation(this@MainActivity, R.anim.fade_slide_out_to_left)
+        slideOutToLeft.duration = animationDuration
+
         if (state == SEARCH_OPEN) {
 
-            llTitleBar.visibility = View.GONE
             llSearchBox.visibility = View.VISIBLE
+
+            llSearchBox.startAnimation(slideInFromLeft)
+            llTitleBar.startAnimation(slideOutToRight)
+
+            Handler().postDelayed({
+                llTitleBar.visibility = View.GONE
+            }, animationDuration)
+
+            etSearchBox.addTextChangedListener(object: TextWatcher {
+                override fun beforeTextChanged(
+                    s: CharSequence?,
+                    start: Int,
+                    count: Int,
+                    after: Int
+                ) {}
+
+                override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+
+                    val searchTerm = s.toString()
+
+                    if (searchTerm != previousSearchTerm) {
+                        previousSearchTerm = searchTerm
+
+                        searchRunnable?.let { searchHandler.removeCallbacks(it) }
+
+                        searchRunnable = Runnable {
+                            handleMessage(TAG_RESPONSE_CONTACT, searchTerm)
+                        }
+
+                        searchRunnable?.let { searchHandler.postDelayed(it, searchDelayMillis) }
+
+                    }
+
+                }
+
+                override fun afterTextChanged(s: Editable?) {}
+
+            })
 
         } else {
 
             llTitleBar.visibility = View.VISIBLE
-            llSearchBox.visibility = View.GONE
+
+            llTitleBar.startAnimation(slideInFromRight)
+            llSearchBox.startAnimation(slideOutToLeft)
+
+            Handler().postDelayed({
+                llSearchBox.visibility = View.GONE
+            }, animationDuration)
+
+            if (etSearchBox.text.toString() != "") etSearchBox.setText("")
 
         }
-
-    }
-
-    override fun onResume() {
-
-        super.onResume()
-        setRecyclerView()
-        toggleSearchEvent(SEARCH_CLOSE)
 
     }
 
