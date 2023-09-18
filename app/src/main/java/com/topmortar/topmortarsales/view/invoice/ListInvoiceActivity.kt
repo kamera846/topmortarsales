a1e2642be154915fb907ce1441f65e08e2017d13
Mochammad Rafli Ramadani <keerouk.ink@gmail.com>
2023-09-18 16:59:43 +0700

    setup filter invoice

--- app/src/main/java/com/topmortar/topmortarsales/view/invoice/ListInvoiceActivity.kt
+++ app/src/main/java/com/topmortar/topmortarsales/view/invoice/ListInvoiceActivity.kt
@@ -1,17 +1,22 @@
 package com.topmortar.topmortarsales.view.invoice
 
 import android.content.Intent
+import android.content.res.Configuration
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.view.Gravity
+import android.view.MenuItem
 import android.view.View
 import android.view.animation.Animation
 import android.view.animation.AnimationUtils
 import android.widget.EditText
 import android.widget.ImageView
 import android.widget.LinearLayout
+import android.widget.PopupMenu
 import android.widget.RelativeLayout
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -24,14 +29,20 @@ import com.topmortar.topmortarsales.commons.CONST_NAME
 import com.topmortar.topmortarsales.commons.MANAGE_USER_ACTIVITY_REQUEST_CODE
 import com.topmortar.topmortarsales.commons.RESPONSE_STATUS_EMPTY
 import com.topmortar.topmortarsales.commons.RESPONSE_STATUS_OK
+import com.topmortar.topmortarsales.commons.SEARCH_OPEN
 import com.topmortar.topmortarsales.commons.SYNC_NOW
 import com.topmortar.topmortarsales.commons.TAG_RESPONSE_CONTACT
+import com.topmortar.topmortarsales.commons.TOAST_SHORT
+import com.topmortar.topmortarsales.commons.USER_KIND_ADMIN
 import com.topmortar.topmortarsales.commons.utils.SessionManager
 import com.topmortar.topmortarsales.commons.utils.convertDpToPx
 import com.topmortar.topmortarsales.commons.utils.handleMessage
 import com.topmortar.topmortarsales.data.ApiService
 import com.topmortar.topmortarsales.data.HttpClient
 import com.topmortar.topmortarsales.model.InvoiceModel
+import com.topmortar.topmortarsales.view.city.ManageCityActivity
+import com.topmortar.topmortarsales.view.skill.ManageSkillActivity
+import com.topmortar.topmortarsales.view.user.ManageUserActivity
 import kotlinx.coroutines.launch
 
 @Suppress("DEPRECATION")
@@ -44,20 +55,26 @@ class ListInvoiceActivity : AppCompatActivity(), InvoiceRecyclerViewAdapter.Item
     private lateinit var txtLoading: TextView
     private lateinit var titleBar: TextView
     private lateinit var titleBarDescription: TextView
+    private lateinit var tvFilter: TextView
     private lateinit var rvListItem: RecyclerView
     private lateinit var llTitleBar: LinearLayout
     private lateinit var llSearchBox: LinearLayout
+    private lateinit var llFilter: LinearLayout
     private lateinit var icBack: ImageView
     private lateinit var icSearch: ImageView
     private lateinit var icSyncNow: ImageView
+    private lateinit var icOption: ImageView
     private lateinit var icCloseSearch: ImageView
     private lateinit var icClearSearch: ImageView
     private lateinit var etSearchBox: EditText
 
     // Global
     private lateinit var sessionManager: SessionManager
+    private var isListActive: String = LIST_SURAT_JALAN
+    private var isFilterInvoice: String = FILTER_NONE
     private var doubleBackToExitPressedOnce = false
     private var contactId: String? = null
+    private var iName: String? = null
 
     // Initialize Search Engine
     private val searchDelayMillis = 500L
@@ -65,6 +82,14 @@ class ListInvoiceActivity : AppCompatActivity(), InvoiceRecyclerViewAdapter.Item
     private var searchRunnable: Runnable? = null
     private var previousSearchTerm = ""
     private var isSearchActive = false
+
+    companion object {
+        const val LIST_SURAT_JALAN = "list_surat_jalan"
+        const val LIST_INVOICE = "list_invoice"
+        const val FILTER_NONE = "list_none"
+        const val FILTER_PAID = "list_paid"
+        const val FILTER_UNPAID = "list_unpaid"
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
@@ -92,20 +117,34 @@ class ListInvoiceActivity : AppCompatActivity(), InvoiceRecyclerViewAdapter.Item
         rvListItem = findViewById(R.id.rv_chat_list)
         llTitleBar = findViewById(R.id.title_bar)
         llSearchBox = findViewById(R.id.search_box)
+        llFilter = findViewById(R.id.ll_filter)
         icBack = llTitleBar.findViewById(R.id.ic_back)
         icSearch = llTitleBar.findViewById(R.id.ic_search)
         icSyncNow = llTitleBar.findViewById(R.id.ic_sync_now)
+        icOption = llTitleBar.findViewById(R.id.ic_more)
         titleBar = llTitleBar.findViewById(R.id.tv_title_bar)
         titleBarDescription = llTitleBar.findViewById(R.id.tv_title_bar_description)
+        tvFilter = findViewById(R.id.tv_filter)
         icCloseSearch = findViewById(R.id.ic_close_search)
         icClearSearch = findViewById(R.id.ic_clear_search)
         etSearchBox = findViewById(R.id.et_search_box)
 
         // Set Title Bar
         icBack.visibility = View.VISIBLE
-        icSyncNow.visibility = View.VISIBLE
-        titleBar.text = "Surat Jalan"
-        titleBar.setPadding(0, 0, convertDpToPx(16, this), 0)
+
+        if (sessionManager.userKind() == USER_KIND_ADMIN) icOption.visibility = View.VISIBLE
+        else icSyncNow.visibility = View.VISIBLE
+
+        // Get the current theme mode (light or dark)
+        val currentNightMode = resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK
+        if (currentNightMode == Configuration.UI_MODE_NIGHT_YES) llFilter.background = getDrawable(R.color.black_400)
+        else llFilter.background = getDrawable(R.color.light)
+
+        val padding16 = convertDpToPx(16, this)
+//        val padding8 = convertDpToPx(8, this)
+        titleBar.setPadding(0, 0, padding16, 0)
+//        icSyncNow.setPadding(padding16, 0, 0, 0)
+//        icOption.setPadding(padding8, 0, padding16, 0)
 
     }
 
@@ -113,27 +152,29 @@ class ListInvoiceActivity : AppCompatActivity(), InvoiceRecyclerViewAdapter.Item
 
         icBack.setOnClickListener { finish() }
         icSyncNow.setOnClickListener { getList() }
+        icOption.setOnClickListener { showPopupMenu() }
+        llFilter.setOnClickListener { showDropdownMenu() }
 
     }
 
     private fun dataActivityValidation() {
 
         contactId = intent.getStringExtra(CONST_CONTACT_ID)
-        val iName = intent.getStringExtra(CONST_NAME)
-
-        if (!iName.isNullOrEmpty()) {
-            titleBar.text = iName
-            titleBarDescription.text = "List surat jalan pada toko ini"
-            titleBarDescription.visibility = View.VISIBLE
-        } else {
-            titleBar.text = "Surat Jalan"
-            titleBarDescription.text = ""
-            titleBarDescription.visibility = View.GONE
-        }
+        iName = intent.getStringExtra(CONST_NAME)
 
     }
 
     private fun getList() {
+        llFilter.visibility = View.GONE
+        titleBarDescription.visibility = View.VISIBLE
+
+        if (isListActive == LIST_SURAT_JALAN) {
+            titleBar.text = if (!iName.isNullOrEmpty()) iName else "Surat Jalan"
+            titleBarDescription.text = "Daftar surat jalan pada toko ini"
+        } else if (isListActive == LIST_INVOICE) {
+            titleBar.text = if (!iName.isNullOrEmpty()) iName else "Invoice"
+            titleBarDescription.text = "Daftar invoice pada toko ini"
+        }
 
         loadingState(true)
 
@@ -148,7 +189,15 @@ class ListInvoiceActivity : AppCompatActivity(), InvoiceRecyclerViewAdapter.Item
 
                         setRecyclerView(response.results)
                         loadingState(false)
-//                        loadingState(true, "Success get data!")
+
+                        if (isListActive == LIST_INVOICE) {
+                            llFilter.visibility = View.VISIBLE
+                            when (isFilterInvoice) {
+                                FILTER_PAID -> tvFilter.text = "Paid"
+                                FILTER_UNPAID -> tvFilter.text = "Unpaid"
+                                else -> tvFilter.text = "None"
+                            }
+                        }
 
                     }
                     RESPONSE_STATUS_EMPTY -> {
@@ -202,6 +251,67 @@ class ListInvoiceActivity : AppCompatActivity(), InvoiceRecyclerViewAdapter.Item
                     } else lastScrollPosition = -1
                 }
             })
+        }
+    }
+
+    private fun showPopupMenu() {
+
+        val popupMenu = PopupMenu(this@ListInvoiceActivity, icOption)
+        popupMenu.inflate(R.menu.option_invoice_menu)
+
+        popupMenu.setOnMenuItemClickListener { item: MenuItem ->
+            when (item.itemId) {
+                R.id.option_sync_now -> {
+                    getList()
+                    true
+                }
+                R.id.option_surat_jalan -> {
+                    toggleList(LIST_SURAT_JALAN)
+                    true
+                }
+                R.id.option_invoices -> {
+                    toggleList(LIST_INVOICE)
+                    true
+                }
+                else -> false
+            }
+        }
+        popupMenu.show()
+    }
+
+    private fun showDropdownMenu() {
+        val popupMenu = PopupMenu(this, llFilter, Gravity.END)
+        popupMenu.menuInflater.inflate(R.menu.option_filter_invoice_menu, popupMenu.menu)
+
+        popupMenu.setOnMenuItemClickListener { item: MenuItem? ->
+            when (item?.itemId) {
+                R.id.option_none -> {
+                    toggleFilter(FILTER_NONE)
+                    return@setOnMenuItemClickListener  true
+                } R.id.option_paid -> {
+                    toggleFilter(FILTER_PAID)
+                    return@setOnMenuItemClickListener  true
+                } R.id.option_unpaid -> {
+                    toggleFilter(FILTER_UNPAID)
+                    return@setOnMenuItemClickListener  true
+                } else -> return@setOnMenuItemClickListener false
+            }
+        }
+
+        popupMenu.show()
+    }
+
+    private fun toggleList(list: String) {
+        if (list != isListActive) {
+            isListActive = list
+            getList()
+        }
+    }
+
+    private fun toggleFilter(filter: String) {
+        if (filter != isFilterInvoice) {
+            isFilterInvoice = filter
+            getList()
         }
     }
 
