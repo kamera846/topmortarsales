c5b1deb355a0f742b0bff44711ac9b5621c69494
Mochammad Rafli Ramadani <keerouk.ink@gmail.com>
2023-09-21 22:08:20 +0700

    clean code

--- app/src/main/java/com/topmortar/topmortarsales/view/MapsActivity.kt
+++ app/src/main/java/com/topmortar/topmortarsales/view/MapsActivity.kt
@@ -2,19 +2,16 @@ package com.topmortar.topmortarsales.view
 
 import android.Manifest
 import android.annotation.SuppressLint
+import android.content.IntentSender
 import android.content.pm.PackageManager
 import android.location.Address
 import android.location.Geocoder
 import android.location.Location
 import android.location.LocationListener
-import android.os.Build
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
 import android.widget.Button
-import android.widget.EditText
 import android.widget.Toast
-import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import com.google.android.gms.common.ConnectionResult
@@ -22,7 +19,6 @@ import com.google.android.gms.common.api.GoogleApiClient
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationRequest
 import com.google.android.gms.location.LocationServices
-
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
@@ -31,20 +27,28 @@ import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.Marker
 import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.libraries.places.api.Places
+import com.google.android.libraries.places.api.model.Place
+import com.google.android.libraries.places.api.net.FetchPlaceRequest
+import com.google.android.libraries.places.api.net.PlacesClient
 import com.topmortar.topmortarsales.R
+import com.topmortar.topmortarsales.commons.CONNECTION_FAILURE_RESOLUTION_REQUEST
+import com.topmortar.topmortarsales.commons.LOCATION_PERMISSION_REQUEST_CODE
 import com.topmortar.topmortarsales.commons.TOAST_LONG
 import com.topmortar.topmortarsales.commons.TOAST_SHORT
 import com.topmortar.topmortarsales.commons.utils.URLUtility
 import com.topmortar.topmortarsales.databinding.ActivityMapsBinding
 import java.io.IOException
+import java.util.Locale
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback, LocationListener, GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
 
     private lateinit var mMap: GoogleMap
     private lateinit var binding: ActivityMapsBinding
     private lateinit var fusedLocationClient: FusedLocationProviderClient
+    private lateinit var placesClient: PlacesClient
 
-    private val locationPermissionCode = 1
+    private val zoomLevel = 16f
     private var selectedLocation: LatLng? = null
     private var currentLatLng: LatLng? = null
 
@@ -64,29 +68,36 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback, LocationListener, 
         mapFragment.getMapAsync(this)
 
         fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+        if (!Places.isInitialized()) {
+            Places.initialize(applicationContext, getString(R.string.maps_key))
+        }
+        placesClient = Places.createClient(this)
 
         onCalculate()
-        onFindLocation()
-//        searchLocation()
+//        onFindLocation()
+        searchLocation()
     }
 
     private fun onFindLocation() {
-        binding.btnFindLocation.setOnClickListener {
+        binding.btnSearchPlace.setOnClickListener {
 //            val mapsUrl = "https://maps.app.goo.gl/CNguhADL5yhGLNTn8"
-            val mapsUrl = "${ binding.inputMapsUrl.text }"
+            val mapsUrl = "${ binding.inputSearchPlace.text }"
             val urlUtility = URLUtility(this)
 
             urlUtility.fetchOriginalUrl(mapsUrl) { originalUrl ->
+
                 if (originalUrl.isNotEmpty()) {
+
                     val latLng = urlUtility.getLatLng(originalUrl)
-                    if (latLng != null) initMaps(latLng)
-                    else {
+                    if (latLng == null) {
                         val placeName = getPlaceNameFromMapsUrl(originalUrl)
                         if (!placeName.isNullOrEmpty()) searchLocation(placeName)
                         else Toast.makeText(this, "Failed to find coordinate", TOAST_SHORT).show()
-                    }
-//                    Toast.makeText(this, "$originalUrl", TOAST_LONG).show()
+
+                    } else initMaps(latLng)
+
                 } else Toast.makeText(this, "Failed to process the URL", TOAST_SHORT).show()
+
             }
 
         }
@@ -96,58 +107,71 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback, LocationListener, 
         val startIndex = mapsUrl.indexOf("/place/") + "/place/".length
         val endIndex = mapsUrl.indexOf("/data=")
 
-        if (startIndex != -1 && endIndex != -1) {
-            return mapsUrl.substring(startIndex, endIndex).replace("+", " ")
-        }
+        if (startIndex != -1 && endIndex != -1) return mapsUrl.substring(startIndex, endIndex).replace("+", " ")
 
         return null
     }
 
+    private fun getPlaceNameFromLatLng(latLng: LatLng?): String {
+
+        val geocoder = Geocoder(this, Locale.getDefault())
+        val addresses = geocoder.getFromLocation(latLng!!.latitude, latLng.longitude, 1)
+
+        return if (addresses!!.isNotEmpty()) addresses[0].getAddressLine(0) else "Location Name Not Found"
+
+    }
+
 
     private fun onCalculate() {
+
         val confirmLocationButton = findViewById<Button>(R.id.confirmLocationButton)
         confirmLocationButton.setOnClickListener {
+
             if (selectedLocation != null) {
+
                 val urlUtility = URLUtility(this)
                 val distance = urlUtility.calculateDistance(currentLatLng!!.latitude, currentLatLng!!.longitude, selectedLocation!!.latitude, selectedLocation!!.longitude)
                 val shortDistance = "%.3f".format(distance).toDouble()
 
-                val message: String = if (distance > 0.2) "$shortDistance Maaf, jarak anda telah melebihi 200 meter."
-                else "$shortDistance Jarak anda sudah lebih dekat dari 200 meter."
+                val message: String = if (distance > 0.2) "$shortDistance Sorry, your distance has exceeded 200 meters."
+                else "$shortDistance Congratulation, your distance is closer than 200 meters."
 
                 Toast.makeText(this@MapsActivity, message, TOAST_LONG).show()
-            } else {
-                Toast.makeText(this, "Pilih lokasi terlebih dahulu", Toast.LENGTH_SHORT).show()
-            }
+
+            } else Toast.makeText(this, "Not selected location", Toast.LENGTH_SHORT).show()
+
         }
+
     }
 
     @SuppressLint("MissingPermission")
     private fun initMaps(latLng: LatLng? = null) {
-        if (latLng != null) {
-            selectedLocation = latLng
-            mMap.clear() // Hapus marker sebelumnya
-            mMap.addMarker(
-                MarkerOptions()
-                    .position(selectedLocation!!)
-                    .title("Selected Location")
-            )
-            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(selectedLocation!!, mMap.maxZoomLevel))
-        } else {
+
+        if (latLng != null) setPin(latLng, getPlaceNameFromLatLng(latLng))
+        else {
             fusedLocationClient.lastLocation
                 .addOnSuccessListener { location: Location? ->
                     if (location != null) {
                         currentLatLng = LatLng(location.latitude, location.longitude)
-                        selectedLocation = currentLatLng
-                        mMap.addMarker(
-                            MarkerOptions()
-                                .position(currentLatLng!!)
-                                .title("My Current Location")
-                        )
-                        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(currentLatLng!!, mMap.maxZoomLevel))
+                        setPin(currentLatLng!!, "My Current Location")
                     }
                 }
         }
+
+    }
+
+    private fun setPin(latLng: LatLng, placeName: String, moveCamera: Boolean = true) {
+
+        selectedLocation = latLng
+        mMap.clear()
+        mMap.addMarker(
+            MarkerOptions()
+                .position(latLng)
+                .title(placeName)
+        )
+
+        if (moveCamera) mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, zoomLevel))
+
     }
 
     override fun onRequestPermissionsResult(
@@ -156,7 +180,7 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback, LocationListener, 
         grantResults: IntArray
     ) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        if (requestCode == locationPermissionCode) {
+        if (requestCode == LOCATION_PERMISSION_REQUEST_CODE) {
             if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                 initMaps()
             }
@@ -171,25 +195,21 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback, LocationListener, 
                 Manifest.permission.ACCESS_FINE_LOCATION
             ) == PackageManager.PERMISSION_GRANTED
         ) {
-            mMap.isMyLocationEnabled = true // Aktifkan tampilan lokasi perangkat saat ini
-            mMap.uiSettings.isMyLocationButtonEnabled = true // Aktifkan tombol lokasi perangkat saat ini
+            mMap.isMyLocationEnabled = true
+            mMap.uiSettings.isCompassEnabled = true
+            mMap.uiSettings.isZoomControlsEnabled = true
+            mMap.uiSettings.isTiltGesturesEnabled = true
+            mMap.uiSettings.isMyLocationButtonEnabled = true
 
             initMaps()
 
-            mMap.setOnMapClickListener { latLng ->
-                selectedLocation = latLng
-                mMap.clear() // Hapus marker sebelumnya
-                mMap.addMarker(
-                    MarkerOptions()
-                        .position(selectedLocation!!)
-                        .title("Selected Location")
-                )
-            }
+            mMap.setOnMapLongClickListener { latLng -> setPin(latLng, getPlaceNameFromLatLng(latLng), moveCamera = false) }
+
         } else {
             ActivityCompat.requestPermissions(
                 this,
                 arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
-                locationPermissionCode
+                LOCATION_PERMISSION_REQUEST_CODE
             )
         }
         buildGoogleApiClient()
@@ -204,63 +224,69 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback, LocationListener, 
         mGoogleApiClient!!.connect()
     }
 
-    private fun searchLocation(placeName: String?) {
+    @SuppressLint("MissingPermission")
+    private fun searchLocation(placeName: String? = null) {
         if (!placeName.isNullOrEmpty()) {
+
             var addressList: List<Address>? = null
 
             val geoCoder = Geocoder(this)
-            try {
-                addressList = geoCoder.getFromLocationName(placeName, 1)
-            }catch (e: IOException){
-                e.printStackTrace()
-            }
+            try { addressList = geoCoder.getFromLocationName(placeName, 1) }
+            catch (e: IOException) { e.printStackTrace() }
 
             val address = addressList!![0]
             val latLng = LatLng(address.latitude, address.longitude)
-            mMap.addMarker(MarkerOptions().position(latLng).title(placeName))
-            mMap.animateCamera(CameraUpdateFactory.newLatLng(latLng))
-        }
-        binding.btnFindLocation.setOnClickListener {
-            val location = "${ binding.inputMapsUrl.text.trim() }"
-            var addressList: List<Address>? = null
 
-            if (location.isNullOrEmpty()){
-                Toast.makeText(this, "Failed to find location", Toast.LENGTH_SHORT).show()
-            } else{
-                val geoCoder = Geocoder(this)
-                try {
-                    addressList = geoCoder.getFromLocationName(location, 1)
-                }catch (e: IOException){
-                    e.printStackTrace()
+            setPin(latLng, getPlaceNameFromLatLng(latLng))
+
+        } else {
+
+            binding.btnSearchPlace.setOnClickListener {
+
+                val location = "${binding.inputSearchPlace.text.trim()}"
+                var addressList: List<Address>? = null
+                if (location.isNullOrEmpty()) Toast.makeText(this, "Failed to find location", Toast.LENGTH_SHORT).show()
+                else {
+
+                    val geoCoder = Geocoder(this)
+
+                    try {
+                        addressList = geoCoder.getFromLocationName(location, 1)
+                        val name = addressList!![0].getAddressLine(0)
+
+                    } catch (e: IOException) {
+                        e.printStackTrace()
+                    }
+
+                    val address = addressList!![0]
+                    val latLng = LatLng(address.latitude, address.longitude)
+
+                    setPin(latLng, getPlaceNameFromLatLng(latLng))
+
                 }
-
-                val address = addressList!![0]
-                val latLng = LatLng(address.latitude, address.longitude)
-                mMap.addMarker(MarkerOptions().position(latLng).title(location))
-                mMap.animateCamera(CameraUpdateFactory.newLatLng(latLng))
             }
+
         }
     }
 
     override fun onLocationChanged(location: Location) {
         mLastLocation = location
-        if (mCurrLocationMarker != null){
-            mCurrLocationMarker!!.remove()
-        }
+
+        if (mCurrLocationMarker != null) mCurrLocationMarker!!.remove()
 
         val latLng = LatLng(location.latitude, location.longitude)
         val markerOptions = MarkerOptions()
         markerOptions.position(latLng)
         markerOptions.title("Current Position")
         markerOptions.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN))
+
         mCurrLocationMarker = mMap.addMarker(markerOptions)
 
         mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng))
-        mMap.moveCamera(CameraUpdateFactory.zoomTo(11f))
+        mMap.moveCamera(CameraUpdateFactory.zoomTo(zoomLevel))
 
-        if (mGoogleApiClient != null){
-            LocationServices.getFusedLocationProviderClient(this)
-        }
+        if (mGoogleApiClient != null) LocationServices.getFusedLocationProviderClient(this)
+
     }
 
     override fun onConnected(p0: Bundle?) {
@@ -277,11 +303,14 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback, LocationListener, 
         }
     }
 
-    override fun onConnectionSuspended(p0: Int) {
+    override fun onConnectionSuspended(p0: Int) { mGoogleApiClient?.connect() }
 
-    }
+    override fun onConnectionFailed(connectionResult: ConnectionResult) {
+        if (connectionResult.hasResolution()) {
 
-    override fun onConnectionFailed(p0: ConnectionResult) {
+            try { connectionResult.startResolutionForResult(this, CONNECTION_FAILURE_RESOLUTION_REQUEST) }
+            catch (e: IntentSender.SendIntentException) { e.printStackTrace() }
 
+        } else Toast.makeText(this, "Connection to Google Play Services failed", TOAST_SHORT).show()
     }
 }
