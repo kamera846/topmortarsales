f338c2ce181a0c6eefebe40d3eec9affeb2afa55
M Rafli Ramadani <keerouk.ink@gmail.com>
2024-05-02 14:36:26 +0700

    Setup user creator store name

--- app/src/main/java/com/topmortar/topmortarsales/commons/const.kt
+++ app/src/main/java/com/topmortar/topmortarsales/commons/const.kt
@@ -79,6 +79,7 @@ const val DELIVERY = "delivery.php"
 const val SURAT_JALAN_NOT_CLOSING = "suratjalanNotClosing.php"
 const val RENCANA_VISIT = "rencanaVisit.php"
 const val RENCANA_VISIT_PENAGIHAN = "renvisPenagihan.php"
+const val GET_CONTACT_SALES = "getContactSales.php"
 
 /*
     Request Code
