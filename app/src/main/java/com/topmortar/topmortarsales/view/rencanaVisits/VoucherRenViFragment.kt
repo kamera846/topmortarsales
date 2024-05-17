94a6becdcbce4ee9db1d64aef047ffe764a85d05
M Rafli Ramadani <keerouk.ink@gmail.com>
2024-05-17 14:50:50 +0700

    Swipe refresh on renvi sales

--- app/src/main/java/com/topmortar/topmortarsales/view/rencanaVisits/VoucherRenViFragment.kt
+++ app/src/main/java/com/topmortar/topmortarsales/view/rencanaVisits/VoucherRenViFragment.kt
@@ -106,6 +106,11 @@ class VoucherRenViFragment : Fragment() {
         if (userKind == USER_KIND_ADMIN || userKind == USER_KIND_PENAGIHAN) getCities()
         getList()
 
+        binding.swipeRefreshLayout.setOnRefreshListener {
+            if (userKind == USER_KIND_ADMIN || userKind == USER_KIND_PENAGIHAN) getCities()
+            getList()
+        }
+
         return view
     }
 
@@ -227,10 +232,14 @@ class VoucherRenViFragment : Fragment() {
             binding.txtLoading.visibility = View.VISIBLE
             binding.rvChatList.visibility = View.GONE
 
+            binding.swipeRefreshLayout.isRefreshing = message === getString(R.string.txt_loading)
+
         } else {
 
             binding.txtLoading.visibility = View.GONE
             binding.rvChatList.visibility = View.VISIBLE
+
+            binding.swipeRefreshLayout.isRefreshing = false
 
         }
 
