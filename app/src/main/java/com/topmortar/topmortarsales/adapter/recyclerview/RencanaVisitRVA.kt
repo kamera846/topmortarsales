db1154346e482011f4bdf28f8727ebee974113d2
M Rafli Ramadani <keerouk.ink@gmail.com>
2024-06-06 14:13:39 +0700

    Fix renvi counter

--- app/src/main/java/com/topmortar/topmortarsales/adapter/recyclerview/RencanaVisitRVA.kt
+++ app/src/main/java/com/topmortar/topmortarsales/adapter/recyclerview/RencanaVisitRVA.kt
@@ -44,12 +44,12 @@ class RencanaVisitRVA (private val listItem: ArrayList<RencanaVisitModel>, priva
             }
 
             val dateCounter = DateFormat.differenceDateNowDescCustom(item.created_at)
-            val formatDateCounter = when {
-                dateCounter == 0 -> "hari ini"
-                dateCounter == 1 -> "kemarin"
-                dateCounter > 1 -> "$dateCounter hari"
-                else -> ""
-            }
+//            val formatDateCounter = when {
+//                dateCounter == 0 -> "hari ini"
+//                dateCounter == 1 -> "kemarin"
+//                dateCounter > 1 -> "$dateCounter hari"
+//                else -> ""
+//            }
 
             if (dateCounter > 4 && typeRencana == "jatemPenagihan") itemView.setBackgroundColor(context!!.getColor(R.color.primary15))
             else itemView.setBackgroundColor(context!!.getColor(R.color.baseBackground))
@@ -60,14 +60,14 @@ class RencanaVisitRVA (private val listItem: ArrayList<RencanaVisitModel>, priva
                 else -> "Jatuh tempo "
             }
 
-            dateJatem += if (item.termin_payment.isNotEmpty() && (typeRencana == "jatem" || typeRencana == "jatemPenagihan")) {
+            dateJatem += if (item.termin_payment.isNotEmpty() && item.termin_payment != "-1" && (typeRencana == "jatem" || typeRencana == "jatemPenagihan")) {
                 val terminPayment = item.termin_payment.toInt()
                 DateFormat.changeDateToDaysBeforeOrAfter(item.date_invoice, terminPayment, outputDateFormat = "dd MMMM yyyy")
             } else DateFormat.format(item.created_at)
 
             tvContactName.text = item.nama
             tvPhoneNumber.text = dateJatem
-            textVerified.text = formatDateCounter
+            textVerified.text = "$dateCounter hari"
             textVerified.setBackgroundResource(R.drawable.bg_passive_round)
             textVerified.visibility = View.VISIBLE
 
