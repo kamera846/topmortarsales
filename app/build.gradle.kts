cf7e9427aff23b64c5e33fceb57e3ec4b92e6024
mochammadrafliramadani <mochammadrafliramadani@gmail.com>
2026-04-08 22:25:52 +0700

    update 3.4.260408179

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,8 +29,8 @@ android {
         applicationId = "com.topmortar.topmortarsales"
         minSdk = 24
         targetSdk = 36
-        versionCode = 178
-        versionName = "3.4.260408178"
+        versionCode = 179
+        versionName = "3.4.260408179"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         manifestPlaceholders["MAPS_API_KEY"] = mapsApiKey
